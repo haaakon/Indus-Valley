eafb20295f9c9067d034b0fd2785534534bb44ba
Håkon Bogen <hakon.bogen@shortcut.no>
2015-04-19 12:46:27 +0200

    Initial Commit

---
+++ Indus Valley/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Indus Valley
+//
+//  Created by Håkon Bogen on 19/04/15.
+//  Copyright (c) 2015 haaakon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Indus ValleyTests/Indus_ValleyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Indus_ValleyTests.swift
+//  Indus ValleyTests
+//
+//  Created by Håkon Bogen on 19/04/15.
+//  Copyright (c) 2015 haaakon. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Indus_ValleyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
