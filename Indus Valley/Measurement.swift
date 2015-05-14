aa89cd017f17cd382af7b57273407c642992b551
Håkon Bogen <hakon.bogen@shortcut.no>
2015-05-14 22:46:12 +0200

    rename UnitConstruct to be more clear on what the class does

--- Indus Valley/Measurement.swift
+++ Indus Valley/Measurement.swift
@@ -1,36 +1,26 @@
 //
-//  Measurement.swift
+//  UnitConstruct.swift
 //  Indus Valley
 //
-//  Created by Håkon Bogen on 19/04/15.
+//  Created by Håkon Bogen on 09/05/15.
 //  Copyright (c) 2015 haaakon. All rights reserved.
 //
 
 import Foundation
 
-//protocol UnitProtocol {
+class UnitConstruct {
+    
+    var quantity: Double
+    var unit : UnitProtocol
 
-//    var quantity : Double { get set }
-//    var unit : String? { get set }
+    init(quantity: Double, unit: UnitProtocol) {
+        self.quantity = quantity
+        self.unit = unit
+    }
 
-//    init(quantity: Double, unit: String)
+    func convert(#toUnit : UnitProtocol) -> UnitConstruct {
+        let newUnit = UnitConstruct(quantity:  self.quantity * self.unit.factor / toUnit.factor, unit: toUnit)
+        return newUnit
+    }
 
-//    static func convert(measurement: Measurement, toUnit unit: String) -> Measurement
-
-//    mutating func convert(#toUnit:String)
-
-    //    mutating func convert(#toUnit: MassUnit) {
-    //        self = Mass.convertMass(self, toUnit: toUnit)
-    //    }
-//}
-
-
-//func +<T: Measurement> (left:T, right:T) -> Measurement {
-//    let newRightValue  = right.convert(toUnit: left.unit)
-//    return T(quantity: left.quantity + newRightValue.quantity , unit: left.unit)
-//}
-
-//func - (left:Mass, right:Mass) -> Mass {
-//    let newRightValue = right.converted(toUnit: left.unit)
-//    return  Mass(quantity: left.quantity - newRightValue.quantity, unit: left.unit)
-//}
+}
